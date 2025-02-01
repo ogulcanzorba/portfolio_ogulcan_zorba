a714b6b3e491e79f666e2e821f688b5de1371ed4
Olci <ogulcan1134@gmail.com>
2025-02-01 20:32:40 +0300

    Fourth Commit
    need some changes, all of are copy paste

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
+import heroImg from "../../assets/olci.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
-import LinkedInLight from "../../assets/linkedin-light.svg";
-import LinkedInDark from "../../assets/linkedin-dark.svg";
-import GithubLight from "../../assets/github-light.svg";
-import GithubDark from "../../assets/github-dark.svg";
+import twitterLight from "../../assets/twitter-light.svg";
+import twitterDark from "../../assets/twitter-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
 import CV from "../../assets/cv.pdf";
 import { useTheme } from "../../common/ThemeContext";
 
@@ -13,16 +15,17 @@ function Hero() {
   const { theme, toggleTheme } = useTheme();
 
   const themeIcon = theme === "light" ? sun : moon;
-  const LinkedInIcon = theme === "light" ? LinkedInLight : LinkedInDark;
-  const GithubIcon = theme === "light" ? GithubLight : GithubDark;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
 
   return (
-    <section id="hero" className="">
+    <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img
-          className={styles.hero}
           src={heroImg}
-          alt="Profile picture of Oğulcan Zorba"
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
         />
         <img
           className={styles.colorMode}
@@ -33,22 +36,25 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Oğulcan
+          Harris
           <br />
-          Zorba
+          Johnsen
         </h1>
-        <h2>CSE Student</h2>
+        <h2>Frontend Developer</h2>
         <span>
-          <a href="https://www.linkedin.com/in/ogulcanzorba" target="_blank">
-            <img src={LinkedInIcon} alt="LinkedIn icon" />
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://www.github.com/ogulcanzorba" target="_blank">
-            <img src={GithubIcon} alt="Github icon" />
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-        <p>
-          CS Engineering student with a passion for creating scalable tech
-          solutions.
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
         </p>
         <a href={CV} download>
           <button className="hover">Resume</button>
